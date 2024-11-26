eecf152fe898e480183468e2837cb8c4a0b403bf
vunghiak3 <vunghiak3@gmail.com>
2024-11-26 22:30:55 +0700

    update episode

--- frontend/src/components/Episode.js
+++ frontend/src/components/Episode.js
@@ -1,15 +1,12 @@
-import { useState } from "react";
 import styles from "./Episode.module.css";
-import { useParams } from "react-router-dom";
 
 export default function Episode({ movie }) {
-  const { slug } = useParams();
   return (
     <div className={styles.episodeDetailMovie}>
       <h2>Tập phim</h2>
       <div className={styles.listButton}>
         {movie.episode?.map((item, index) => (
-          <a href={`${slug}/${item.name}`} key={index}>{item.name}</a>
+          <a href={`/phim/${movie.slug}/${item.name}`} key={index}>{item.name}</a>
         ))}
       </div>
     </div>
